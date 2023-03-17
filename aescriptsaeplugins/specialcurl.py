f1506bc55b78df4ec91cf170811a45248c8439b3
SupportPlan <servicedesk@supportplan.com>
2023-03-17 15:36:19 +0000

    Create specialcurl.py

---
+++ aescriptsaeplugins/specialcurl.py
@@ -0,0 +1,65 @@
+#!/usr/local/autopkg/python
+
+import os
+import shutil
+import subprocess
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["specialcurl"]
+
+
+class specialcurl(Processor):
+    """Unpacks a package payload."""
+
+    input_variables = {
+        "Referer": {
+            "required": True,
+            "description": (
+                "The referer url"
+            ),
+        },
+        "destination_path": {"required": True, "description": "Destination directory."},
+        "url": {
+            "required": True,
+            "description": (
+                "URL to download from"
+            ),
+        },
+    }
+    output_variables = {}
+    description = __doc__
+    
+    def download(self):
+        try:
+            ccmd = [
+                "/usr/bin/curl",
+                "-L",
+                "-H",
+                self.env["Referer"],
+                self.env["url"],
+                "-o",
+                self.env["destination_path"]
+            ]
+            proc = subprocess.Popen(
+                ccmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
+            (_, err_out) = proc.communicate()
+        except OSError as err:
+            error = (
+                f"download failed with error code {err.errno}: {err.strerror}"
+            )
+        if proc.returncode != 0:
+            error = (
+                f"download of {self.env['url']} with curl failed: "
+                f"{err_out}"
+            )
+        self.output("Downloaded sucessfully")
+
+    def main(self):
+        self.download()
+
+
+if __name__ == "__main__":
+    PROCESSOR = specialcurl()
+    PROCESSOR.execute_shell()
